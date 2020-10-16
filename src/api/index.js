825af565f0281e6a3dd4b34bd4f9e2e930348a3b
dnjsskarb7 <64752079+dnjsskarb7@users.noreply.github.com>
2020-10-16 23:59:19 +0900

    a

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+
 const url = `https://covid19.mathdro.id/api`;
 
 export const fetchData = async (country) => {
   let changeableUrl = url;
+
   if (country) {
     changeableUrl = `${url}/countries/${country}`;
   }
